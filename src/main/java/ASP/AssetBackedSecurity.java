2b3a112381c7bb59b6d525f731d5403a08a85f31
Sean <mcquill99@gmail.com>
2020-04-09 21:44:22 -0400

    Failing tests for abs class

--- src/main/java/ASP/AssetBackedSecurity.java
+++ src/main/java/ASP/AssetBackedSecurity.java
@@ -1,4 +1,23 @@
 package ASP;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class AssetBackedSecurity {
+
+    private List<Loan> loans;
+    private double riskValue;
+
+    public AssetBackedSecurity(List<Loan> loansList){
+        loans = loansList;
+    }
+
+    public void generateRiskValue(){
+    }
+
+
+    public double getRiskValue(){
+        return 0;
+    }
+
 }
