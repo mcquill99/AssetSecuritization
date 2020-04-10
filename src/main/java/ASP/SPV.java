0836d05df3939d907310eec0c71916dd29818928
Sean <mcquill99@gmail.com>
2020-04-10 14:57:23 -0400

    created empty spv class

---
+++ src/main/java/ASP/SPV.java
@@ -0,0 +1,25 @@
+package ASP;
+
+import java.util.List;
+
+public class SPV {
+    List<Loan> Loans;
+    List<AssetBackedSecurity> ABSList;
+
+
+    public SPV(){
+
+    }
+
+    public void buyLoan(){
+
+    }
+
+    public void createABS(){
+
+    }
+
+    public void AssignRiskValue(){
+
+    }
+}
--- src/test/java/ABSTest.java
+++ src/test/java/ABSTest.java
@@ -41,8 +41,6 @@ public class ABSTest {
             assertEquals(getAverageInterest(loans), abs.getRiskValue());
         }
 
-
-
     }
 
     @Test
