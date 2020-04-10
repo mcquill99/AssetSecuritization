0dec39e2247d3c56d17b5a5555f47361d9fbbf15
Sean <mcquill99@gmail.com>
2020-04-09 21:46:47 -0400

    implement abs class

--- src/main/java/ASP/AssetBackedSecurity.java
+++ src/main/java/ASP/AssetBackedSecurity.java
@@ -13,11 +13,17 @@ public class AssetBackedSecurity {
     }
 
     public void generateRiskValue(){
+        double totalInterest = 0;
+        for(Loan loan: loans){
+            totalInterest += loan.getInterest();
+        }
+
+        riskValue = totalInterest / loans.size();
     }
 
 
     public double getRiskValue(){
-        return 0;
+        return riskValue;
     }
 
 }
