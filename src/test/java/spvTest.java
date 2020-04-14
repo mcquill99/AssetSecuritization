bd67e8c82477fb34c9dc5bc048be1782ef8bf5b0
Sean <mcquill99@gmail.com>
2020-04-14 14:54:29 -0400

    fixed tests

--- src/test/java/spvTest.java
+++ src/test/java/spvTest.java
@@ -1,9 +1,75 @@
+import ASP.*;
 import org.junit.jupiter.api.Test;
-import ASP.Bank;
-import ASP.SPV;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import ASP.AssetBackedSecurity;
 import ASP.Loan;
+import ASP.SPV;
+import ASP.insufficientLoansException;
 
 public class spvTest {
+
+    public List<Loan> generateLoanList(int numOfLoans, int minInterest, int maxInterest){
+        List<Loan> toReturn = new ArrayList<>();
+        Random r = new Random();
+        for(int i = 0; i < numOfLoans; i++){
+            toReturn.add(new Loan(500, r.nextInt((maxInterest - minInterest)+ 1) + minInterest));
+        }
+
+        return toReturn;
+    }
+
+    public boolean checkABS(double min, double max, AssetBackedSecurity absToCheck){
+        for(Loan loan: absToCheck.getLoanList()){
+            if(loan.getInterest() < (min / 100) || loan.getInterest() > (max / 100)){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    @Test
+    public void createABSTest() throws insufficientLoansException {
+        SPV spv;
+        List<Loan> loans = generateLoanList(5,5,10);
+        spv = new SPV(loans);
+
+        //test for interest values outside of loan scope
+        assertThrows(insufficientLoansException.class, () ->spv.createABS(90,100,100));
+        assertThrows(insufficientLoansException.class, () ->spv.createABS(15,20,100));
+        assertThrows(insufficientLoansException.class, () ->spv.createABS(25,50,100));
+
+        //valid ABS creation test
+        assertTrue(checkABS(5,10,spv.createABS(5,10,1)));
+
+        assertTrue(checkABS(5,10,spv.createABS(5,10,4)));
+
+        //test for no loans that arent already assigned to an abs
+        assertThrows(insufficientLoansException.class, () ->spv.createABS(5,10,1));
+        assertThrows(insufficientLoansException.class, () ->spv.createABS(5,10,100));
+
+
+        //test for maxInterest lower than minInterest
+        assertThrows(IllegalArgumentException.class, () ->spv.createABS(10,5,1));
+        assertThrows(IllegalArgumentException.class, () ->spv.createABS(100,0,1));
+
+        //test for negatives
+        assertThrows(IllegalArgumentException.class, () ->spv.createABS(-5,10,1));
+        assertThrows(IllegalArgumentException.class, () ->spv.createABS(5,-10,1));
+        assertThrows(IllegalArgumentException.class, () ->spv.createABS(-5,-10,1));
+
+        //test for negative numLoans request
+        assertThrows(IllegalArgumentException.class, () ->spv.createABS(5,10,0));
+        assertThrows(IllegalArgumentException.class, () ->spv.createABS(5,10,-5));
+        assertThrows(IllegalArgumentException.class, () ->spv.createABS(5,10,-100));
+
+    }
 
     @Test
     public void buyLoanTest(){
@@ -18,3 +84,6 @@ public class spvTest {
 
     }
 }
+
+
+
