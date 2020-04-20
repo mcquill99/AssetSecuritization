926aa14d50a11e841b65daae47bdfaa02da6a70a
PraveshPatel <ppatel@ithaca.edu>
2020-04-20 14:38:43 -0400

    created constructor

--- src/main/java/ASP/Investor.java
+++ src/main/java/ASP/Investor.java
@@ -1,4 +1,15 @@
 package ASP;
 
+import java.util.HashMap;
+import java.util.List;
+
 public class Investor {
+    private List<AssetBackedSecurity> ABSinvestedIn;
+    private double balance;
+
+    public Investor(double balance, List ABSinvestedIn){
+        this.balance = balance;
+        this.ABSinvestedIn = ABSinvestedIn;
+    }
+
 }
